b7f55fef0bd126ca0d43e04559b65c040a3c599f
lrignacio28 <112179840+lrignacio28@users.noreply.github.com>
2022-09-17 10:30:25 -0400

    Update wordleSolver.java

--- src/wordleSolver.java
+++ src/wordleSolver.java
@@ -1,1 +1,14 @@
+public wordleSolver {
+  public static void main(String[] args) {
+    
+    //read in WordleList text file
+    File myObj = new File("WordList.txt");
+    Scanner reader = new Scanner(myObj);
+    while(reader.hasNextLine()) {
+      
+    }
+    reader.close()
+    
+  }
+}
 
