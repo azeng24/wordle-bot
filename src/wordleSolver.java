340a681e8f2d34a2e26bd0c6484203f90fcecb03
lrignacio28 <112179840+lrignacio28@users.noreply.github.com>
2022-09-17 11:05:23 -0400

    Update wordleSolver.java

--- src/wordleSolver.java
+++ src/wordleSolver.java
@@ -1,8 +1,10 @@
+import java.util.Scanner;
+
 public wordleSolver {
   public static void main(String[] args) {
     
     //declare array for frequency
-    double freqArr = [];
+    double freqArr = new double[26];
     
     //declare ArrayList for words
     ArrayList words = new ArrayList();
@@ -17,7 +19,13 @@ public wordleSolver {
     readerWords.close()
       
     //read in frequency text file
-    File myObj2 = new File("
+    File myObj2 = new File("FreqByLetter.txt");
+    Scanner readFreq = new Scanner(myObj2);
+    int i = 0;
+    while (readerFreq.hasNextLine()) {
+      freqArr[i] = readerFreq.nextLine();
+      i++;
+    }
     
   }
 }
